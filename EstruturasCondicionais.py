c7c06f487e951ef6a663767e60b232f633b5b984
IsraelSouza <israel.felipe1991@gmail.com>
2024-08-01 05:49:39 -0300

    Commit inicial - criação do projeto

---
+++ EstruturasCondicionais.py
@@ -0,0 +1,9 @@
+a = int(input("Informe um número inteiro para variável A: "))
+b = int(input("Informe outro número inteiro para variável B: "))
+
+if a < b:
+    print("A é menor que B")
+elif b < a:
+    print('B é menor que A')
+elif a == b:
+    print('As duas variáveis são iguais')
