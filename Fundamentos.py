1e3f0d442d2d8b0c03e4d3615c8cb9d2d69b6cd5
IsraelSouza <israel.felipe1991@gmail.com>
2024-08-05 22:10:56 -0300

    Update Fundamentos.py

--- Fundamentos.py
+++ Fundamentos.py
@@ -1,11 +1,16 @@
-# Estruturas Condicionais / Declaração de Variáveis e tipos
+import os
 
-a = int(input("Informe um número inteiro para variável A: "))
-b = int(input("Informe outro número inteiro para variável B: "))
+diretorio = os.chdir(input(str('Informe o diretório')))
+print(f'Diretório atual: {os.getcwd()}')
 
-if a < b:
-    print("A é menor que B")
-elif b < a:
-    print('B é menor que A')
-elif a == b:
-    print('As duas variáveis são iguais')
+nome_padrao = input('Qual o padrão de nomes de arquivos usar (sem a extensão)?')
+sf = input('Informe o número do projeto')
+
+for contador, arq in enumerate(os.listdir()):
+    if os.path.isfile(arq):
+        nome_arq, exten_arq = os.path.splitext(arq)
+        nome_arq = nome_padrao + '-' + sf + '-' + str(contador)
+        nome_novo = f'{nome_arq}{exten_arq}'
+        os.rename(arq, nome_novo)
+
+print(f'\nArquivos renomeados.')
