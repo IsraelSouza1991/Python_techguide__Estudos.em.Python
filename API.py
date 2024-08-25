011c69cb84b020bb8641a58b9819653e9bc91450
IsraelSouza <israel.felipe1991@gmail.com>
2024-08-25 11:58:48 -0300

    Create API.py

---
+++ API.py
@@ -0,0 +1,4 @@
+"""[Link do artigo]
+URL=https://www.treinaweb.com.br/blog/consumindo-apis-com-python-parte-1#:~:text=Buscando%20dados%20de%20uma%20API&text=Com%20isso%2C%20estamos%20enviando%20uma,todos%20os%20dados%20daquela%20API.&text=No%20terminal%20do%20PyCharm%2C%20podemos,requisi%C3%A7%C3%A3o%20foi%20feita%20com%20sucesso.
+"""
+
